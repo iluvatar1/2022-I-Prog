55f96534786e2a9336e88b58f070ad4ad9d517b7
Iluvatar <woquendo@gmail.com>
2022-05-31 11:47:59 -0500

    Added some eigen examples

---
+++ 2022-06-01-MatrixLibs-1/eigen-MV.cpp
@@ -0,0 +1,13 @@
+// Second example, slighlty modified to include namespaces explicitly
+#include <iostream>
+#include <eigen3/Eigen/Dense>
+
+int main(int argc, char **argv)
+{
+  Eigen::MatrixXd m = Eigen::MatrixXd::Random(3,3);
+  m = (m + Eigen::MatrixXd::Constant(3,3,1.2)) * 50;
+  std::cout << "m =" << std::endl << m << std::endl;
+  Eigen::VectorXd v(3);
+  v << 1, 2, 3;
+  std::cout << "m * v =" << std::endl << m * v << std::endl;
+}
