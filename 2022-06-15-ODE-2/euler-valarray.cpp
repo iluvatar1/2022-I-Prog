39b18ffb2ba8ec7afc2aed8bb2e998837b00af2e
Iluvatar <woquendo@gmail.com>
2022-06-15 09:45:56 -0500

    Updated rk4

--- 2022-06-15-ODE-2/euler-valarray.cpp
+++ 2022-06-15-ODE-2/euler-valarray.cpp
@@ -1,28 +1,37 @@
 #include <iostream>
-#include <vector>
+#include <valarray>
+#include <string>
 #include <cmath>
 #include <cstdlib>
 
-typedef std::vector<double> state_t; // alias for state type
-
-// constants
-const double W = 3.1237;
+typedef std::valarray<double> state_t; // alias for state type
 
 void initial_conditions(state_t & y);
 void print(const state_t & y, double time);
-void fderiv(const state_t & y, state_t & dydt, double t);
 template <class deriv_t, class system_t, class printer_t>
 void integrate_euler(deriv_t deriv, system_t & y, double tinit, double tend, double dt, printer_t writer);
 
 
-int main(void)
+int main(int argc, char **argv)
 {
-  const double DT = 0.001;
-  const double TF = 4*2*M_PI/W;
+  if (argc != 5) {
+    std::cerr << "ERROR. Usage:\n" << argv[0] << " T0 TF DT W" << std::endl;
+    std::exit(1);
+  }
+  const double T0 = std::atof(argv[1]);
+  const double TF = std::atof(argv[2]);
+  const double DT = std::atof(argv[3]);
+  const double W  = std::atof(argv[4]);
   const int N = 2;
   state_t y(N);
   initial_conditions(y);
-  integrate_euler(fderiv, y, 0.0, TF, DT, print);
+
+  auto fderiv = [W](const state_t & y, state_t & dydt, double t){
+    dydt[0] = y[1];
+    dydt[1] = -W*W*y[0];
+  };
+
+  integrate_euler(fderiv, y, T0, TF, DT, print);
 
   return 0;
 }
@@ -38,25 +47,17 @@ void print(const state_t & y, double time)
   std::cout << time << "\t" << y[0] << "\t" << y[1] << std::endl;
 }
 
-void fderiv(const state_t & y, state_t & dydt, double t)
-{
-  dydt[0] = y[1];
-  dydt[1] = -W*W*y[0];
-}
-
 template <class deriv_t, class system_t, class printer_t>
 void integrate_euler(deriv_t deriv, system_t & y, double tinit, double tend, double dt, printer_t writer)
 {
   int N = y.size();
-  system_t dydt(N, 0.0); dydt.resize(2);
+  system_t dydt(N, 0.0);
   double time = 0;
   int nsteps = (tend - tinit)/dt;
   for(int ii = 0; ii < nsteps; ++ii) {
-	time = tinit + ii*dt;
+    time = tinit + ii*dt;
     deriv(y, dydt, time);
-    for (int ii = 0; ii < N; ++ii) {
-      y[ii] += dt*dydt[ii]; //y[ii] = y[ii] + dt*dydt[ii]; // EULER
-    }
-	print(y, time);
+    y = y + dydt*dt;
+    print(y, time);
   }
 }
