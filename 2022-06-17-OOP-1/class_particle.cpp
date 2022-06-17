0b18b6226b80ae0cb1b0371f0f112ce3b20e06c2
Iluvatar <woquendo@gmail.com>
2022-06-17 07:56:05 -0500

    Added constructor

--- 2022-06-17-OOP-1/class_particle.cpp
+++ 2022-06-17-OOP-1/class_particle.cpp
@@ -2,6 +2,10 @@
 struct Particle {
   std::valarray<double> R{0, 0, 0}, V{0, 0, 0}, F{0, 0, 0};
   double mass{0};
+  Particle(){
+    R = {0, 0, 0}; V = {0, 0, 0}; F = {0, 0, 0}; 
+    mass = 0.0;
+  }
 };
 
 int main(int argc, char **argv)
