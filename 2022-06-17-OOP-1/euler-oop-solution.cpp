b7584d6fd0fa3a568406e691f45654826dc9eee6
Iluvatar <woquendo@gmail.com>
2022-06-17 10:17:39 -0500

    Added leap-frog solution

--- 2022-06-17-OOP-1/euler-oop-solution.cpp
+++ 2022-06-17-OOP-1/euler-oop-solution.cpp
@@ -14,9 +14,6 @@ struct Particle {
   }
 };
 
-void integration_start(Particle & body, double dt);
-void integration_step(Particle & body, double dt);
-void compute_forces(Particle & body);
 void simulate(Particle & body, double dt, int nsteps, const std::string & fname);
 
 int main(int argc, char *argv[]) {
@@ -35,8 +32,6 @@ int main(int argc, char *argv[]) {
   // initial conditions
   body1.R = {0, 0, 4.3};
   body1.V = {0.123, 0.0, 0.98};
-  compute_forces(body1); // Force at time 0
-  integration_start(body1, DT); // Fix initial velocity for leap-frog
 
   // perform simulation
   simulate(body1, DT, NSTEPS, "datos.txt");
@@ -44,33 +39,20 @@ int main(int argc, char *argv[]) {
   return 0;
 }
 
-void integration_start(Particle & body, double dt)
-{
-  body.V = body.V - 0.5*body.F*dt/body.mass;
-}
-
-void integration_step(Particle & body, double dt)
-{
-  body.V = body.V + dt*body.F/body.mass;
-  body.R = body.R + dt*body.V;
-}
-
-void compute_forces(Particle & body)
-{
-  const std::valarray<double> G = {0, 0, -9.81};
-  body.F = 0;
-  body.F += body.mass*G;
-}
-
 void simulate(Particle & body, double dt, int nsteps, const std::string & fname)
 {
+  const std::valarray<double> G = {0, 0, -9.81};
 
   std::ofstream fout(fname);
   fout.precision(15); fout.setf(std::ios::scientific);
 
   for (int ii = 0; ii < nsteps; ++ii) {
-    compute_forces(body);
-    integration_step(body, dt);
+    // compute force
+    body.F = 0;
+    body.F += body.mass*G;
+    // apply Euler
+    body.R = body.R + body.V*dt;
+    body.V = body.V + body.F*dt/body.mass;
     // print
     fout << ii*dt << "\t" << body.R[0] << "\t" << body.R[1] << "\t" << body.R[2] << "\t"
          << body.V[0] << "\t" << body.V[1] << "\t" << body.V[2] << "\t"
