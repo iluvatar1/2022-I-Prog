a10cc89b27d9cf2bc9e5bf2346d5f6eff42eb163
Iluvatar <woquendo@gmail.com>
2022-06-23 23:14:52 -0500

    Added basic rng examples

---
+++ 2022-06-24-RandomNumbers-1/example-rng_mt-histogram.cpp
@@ -0,0 +1,43 @@
+#include <iostream>
+#include <fstream>
+#include <random>
+#include <cstdlib>
+#include <vector>
+
+int main(int argc, char **argv)
+{
+  if (8 != argc) {
+    std::cerr << "Error. Usage:\n" << argv[0] << "SEED SAMPLES A B XMIN XMAX NBINS\n";
+    return 1;
+  }
+  const int SEED = std::atoi(argv[1]);
+  const int SAMPLES = std::atoi(argv[2]);
+  const int A = std::atof(argv[3]);
+  const int B = std::atof(argv[4]);
+  const double XMIN = std::atof(argv[5]);
+  const double XMAX = std::atof(argv[6]);
+  const int NBINS = std::atoi(argv[7]);
+  const double DX = (XMAX-XMIN)/NBINS;
+
+  std::vector<double> histo(NBINS, 0.0);
+  std::mt19937 gen(SEED);
+  std::uniform_real_distribution<double> dist(A, B);
+  std::ofstream fout("data.txt");
+  for (int ii = 0; ii < SAMPLES; ++ii) {
+    double r = dist(gen);
+    fout << r << "\n";
+    int bin = int((r - XMIN)/DX); // compute the bin where the sample lies
+    if (0 <= bin && bin < NBINS) { // check if the bin is included
+      histo[bin]++; // increase the counter in that bin
+    }
+  }
+  fout.close();
+
+  fout.open("histo.txt");
+  for (int ii = 0; ii < NBINS; ii++) {
+    fout << XMIN + ii*DX << "\t" << histo[ii]/(DX*SAMPLES) << "\n";
+  }
+  fout.close();
+
+  return 0;
+}
---
+++ 2022-06-24-RandomNumbers-1/example-rng_mt-uniform.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <fstream>
+#include <random>
+#include <cstdlib>
+#include <vector>
+
+int main(int argc, char **argv)
+{
+  if (4 != argc) {
+    std::cerr << "Error. Usage:\n" << argv[0] << "SEED SAMPLES A B\n";
+    return 1;
+  }
+  const int SEED = std::atoi(argv[1]);;
+  const int SAMPLES = std::atoi(argv[2]);
+  const int A = std::atof(argv[3]);;
+  const int B = std::atof(argv[4]);
+
+  std::mt19937 gen(SEED);
+  std::uniform_real_distribution<double> dist(A, B);
+  std::ofstream fout("data.txt");
+  for (int ii = 0; ii < SAMPLES; ++ii) {
+    double r = dist(gen);
+    fout << r << "\n";
+  }
+  fout.close();
+
+  return 0;
+}
